c75dda0481d7a44153921690848142b0b82df637
AnishaDevops <anishacsc2001@gmail.com>
2025-06-03 16:20:41 +0530

    Add files

--- api/main.py
+++ api/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from router import blog_get
+from Fast_API.api.router import blog
 from router import blog_post
 
 app = FastAPI()
-app.include_router(blog_get.router)
+app.include_router(blog.router)
 app.include_router(blog_post.router)
 
 @app.get('/hello')
--- api/router/blog.py
+++ api/router/blog.py
@@ -11,43 +11,31 @@ router = APIRouter(
 def index():
     return {'message': 'hello world!'}
 
-@router.get('/blog/all')
+@router.get('/all')
 def get_all_blogs():
     return {'message': 'All blogs provided'}
 
-#quary parameter
-@router.get('/blog/{id}/comments/{comments_id}')
+@router.get('/{id}/comments/{comment_id}')
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
-
 class BlogeType(str, Enum):
     short = 'short'
-    story = 'sory'
+    story = 'story'
     howto = 'howto'
 
-@router.get('/blog/type/{type}')
+@router.get('/type/{type}')
 def get_blog_type(type: BlogeType):
-    return {'message': f'Blog type {type}'}
+    return {'message': f'Blog type {type.value}'}
 
-#ststus code and Tages
-# tage is tages = ['blog']
 
-@router.get('/blog/{id}',status_code=status.HTTP_200_OK)
+@router.get('/{id}', status_code=status.HTTP_200_OK)
 def get_blog(id: int, response: Response):
     if id > 5:
         response.status_code = status.HTTP_404_NOT_FOUND
         return {'error': f'Blog {id} not found'}
     else:
-        response.status_code = status.HTTP_200_OK
         return {'message': f'Blog with id {id}'}
-    
-#summery and description
 
-@router.get('/test', 
-         tags=['summery'],
-         summary='testing purpose',
-         description='all ok',
-         response_description='the list of avaliable')
-def summery():
+def test_summary():
     return {'message': 'hello world!'}
